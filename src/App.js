ba47f820db1dba6dc45f73a85352fee6323108a7
lemmusm <lemmusm@gmail.com>
2020-05-29 18:00:46 -0500

    use localstorage to save data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import TaskList from './components/TaskList';
 import AddTaskForm from './components/AddTaskForm';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
+  // get info from localStorage
+  let lsTasks = JSON.parse(localStorage.getItem('todos'));
+  if (!lsTasks) {
+    lsTasks = [];
+  }
+  // set state with info from localstorage
+  const [tasks, setTasks] = useState(lsTasks);
   // ADD NEW TASK TO STATE
   const addTask = (nametask) => {
     const newTask = [...tasks, { nametask }];
     setTasks(newTask);
   };
+  // set item on localStorage when state change
+  useEffect(() => {
+    if (lsTasks) {
+      localStorage.setItem('todos', JSON.stringify(tasks));
+    } else {
+      localStorage.setItem('todos', JSON.stringify([]));
+    }
+  }, [lsTasks, tasks]);
 
   // MARK TASK DONE
   const tasksCheck = (index) => {
