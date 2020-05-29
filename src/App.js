d3383342e64eebbfa9ffc317427e18a14d40170f
lemmusm <lemmusm@gmail.com>
2020-05-29 10:35:44 -0500

    install and config react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/img/logo.png';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App content">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Personal TODO List</p>
+      <Button variant="warning">Learn React</Button>
     </div>
   );
 }
